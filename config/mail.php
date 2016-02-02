962f4b78af92fb4df024574b0a2ac54b233e688c
Craig Lovelock <craiglovelock54@homail.co.uk>
2016-02-02 19:09:27 +0000

    settigns

--- config/mail.php
+++ config/mail.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => "mandrill",
 
     /*
     |--------------------------------------------------------------------------
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => "smtp.mandrillapp.com",
 
     /*
     |--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => "craiglovelock54@hotmail.co.uk",
 
     /*
     |--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
     |
     */
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => "GymXkBwplLiYSOF-YxfyqA",
 
     /*
     |--------------------------------------------------------------------------
--- config/services.php
+++ config/services.php
@@ -20,7 +20,7 @@ return [
     ],
 
     'mandrill' => [
-        'secret' => env('MANDRILL_SECRET'),
+        'secret' => "GymXkBwplLiYSOF-YxfyqA",
     ],
 
     'ses' => [
