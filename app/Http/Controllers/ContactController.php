6d9b3d24476f1c8459368ac9abf6950709c234ee
Craig Lovelock <craiglovelock54@homail.co.uk>
2016-09-29 00:43:09 +0100

    changed email to mailgun

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 use Mail;
 use Response;
-use Mandrill;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -23,51 +22,11 @@ class ContactController extends Controller
             'message' => 'required',
         ]);
 
-        $htmlString = "
-            <html>
-            <head>
-              <title>Website Enquiry From: $request->name</title>
-            </head>
-            <body>
-                <p>From: $request->name</p>
-                <p>Contact E-mail: $request->email</p>
-                <p>Contact Number: $request->number</p>
-                <p>Message:</p>
-                <p>$request->message</p>
-                <br />
-                <hr/>
-                <br />
-                <p>
-                    <strong>
-                        Do not reply to this message directly, instead,
-                        <a href='mailto:$request->email'>click here to reply to: $request->email</a>
-                    </strong>
-                </p>
-            </body>
-            </html>
-        ";
+        Mail::send('emails.contact', ['request' => $request], function ($m) use ($request) {
+           $m->from('noreply@cmrmarine.co.uk', 'CMRMarine.co.uk');
 
-        try {
-            $mandrill = new Mandrill('GymXkBwplLiYSOF-YxfyqA');
-            $message = array(
-                'html' => $htmlString,
-                'subject' => "New Website Enquiry",
-                'from_email' => "noreply@cmrmarine.co.uk",
-                'from_name' => "CMRMarine.co.uk",
-                'to' => array(
-                    array(
-                        'email' => "tim@cmrmarine.co.uk",
-                        'name' =>  "Tim",
-                        'type' => 'to'
-                    )
-                )
-            );
-            $result = $mandrill->messages->send($message);
-            print_r($result);
-        } catch(Mandrill_Error $e) {
-            echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
-            throw $e;
-        }
+           $m->to('tim@cmrmarine.co.uk', "Tim")->subject('New Website Enquiry');
+        });
 
     }
 }
--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,1 +1,21 @@
-test
+<html>
+<head>
+    <title>Website Enquiry From: {{ $request->name }}</title>
+</head>
+<body>
+    <p>From: {{ $request->name }}</p>
+    <p>Contact E-mail: {{ $request->email }}</p>
+    <p>Contact Number: {{ $request->number }}</p>
+    <p>Message:</p>
+    <p>{{ $request->message }}</p>
+    <br />
+    <hr/>
+    <br />
+    <p>
+        <strong>
+            Do not reply to this message directly, instead,
+            <a href='mailto:{{ $request->email }}'>click here to reply to: {{ $request->email }}</a>
+        </strong>
+    </p>
+</body>
+</html>
